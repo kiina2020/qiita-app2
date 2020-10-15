13238a45bd2ddd6f2957f9917a64a9082a2f4ac7
kiina2020 <akinasg@gmail.com>
2020-10-16 03:30:57 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
   <v-row justify="center" align="center">
     <v-col v-for="(item, i) in items" :key="i" cols="12">
-      <v-card hover :to="{ name: 'postId' }" color="#1F7087" dark>
+      <v-card
+        hover
+        :to="{ name: 'postId', params: { postId: item.id } }"
+        color="#1F7087"
+        dark
+        @click="onClickCard(item.id)"
+      >
         <div class="d-flex flex-no-wrap justify-space-between">
           <div>
             <v-card-title class="headline" v-text="item.title" />
-            <nuxt-link
-              :to="{ name: 'userId', params: { userId: item.user.id } }"
-            >
-              <v-card-subtitle v-text="item.user.id" />
-            </nuxt-link>
-
+            <v-card-subtitle v-text="item.user.id" />
             <v-card-text v-text="item.body.slice(0, 150)" />
 
             <v-card-actions>
