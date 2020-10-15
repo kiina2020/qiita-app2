26e47d3a420644a344f8d893fd265a6e248e5e71
kiina2020 <akinasg@gmail.com>
2020-10-16 00:50:36 +0900

    fix index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <v-row justify="center" align="center">
     <v-col v-for="(item, i) in items" :key="i" cols="12">
-      <v-card :color="item.color" dark>
+      <v-card hover :to="{ name: 'postId' }" color="#1F7087" dark>
         <div class="d-flex flex-no-wrap justify-space-between">
           <div>
             <v-card-title class="headline" v-text="item.title" />
 
-            <v-card-subtitle v-text="item.artist" />
+            <v-card-subtitle v-text="item.userId" />
+
+            <v-card-text v-text="item.body" />
 
             <v-card-actions>
               <!-- action -->
@@ -14,6 +16,12 @@
           </div>
         </div>
       </v-card>
+    </v-col>
+    <v-col>
+      <!-- <v-btn v-if="currentPage!==1" @click="showPrev" elevation="2">Prev</v-btn> -->
+      <v-btn elevation="2">Prev</v-btn>
+      <v-btn elevation="2">Next</v-btn>
+      <!-- <v-btn @click="showNext" elevation="2">Next</v-btn> -->
     </v-col>
   </v-row>
 </template>
@@ -23,14 +31,24 @@ export default {
   data: () => ({
     items: [
       {
-        color: '#1F7087',
-        title: 'Supermodel',
-        artist: 'Foster the People'
+        title: 'item.title',
+        body: 'item.body.slice(0,150)',
+        userId: 'item.user.id'
       },
       {
-        color: '#952175',
+        title: 'item.title',
+        body: 'item.body.slice(0,150)',
+        userId: 'item.user.id'
+      },
+      {
+        title: 'item.title',
+        body: 'item.body.slice(0,150)',
+        userId: 'item.user.id'
+      },
+      {
         title: 'Halcyon Days',
-        artist: 'Ellie Goulding'
+        body: 'item.body.slice(0,150)',
+        userId: 'Ellie Goulding'
       }
     ]
   })
