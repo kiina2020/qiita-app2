a3711944a47e75b751066baa5f1ccad9e468d6f3
kiina2020 <akinasg@gmail.com>
2020-10-18 17:43:14 +0900

    add login and completed

---
+++ pages/complete.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login completed！</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-card-actions>
+            <v-btn class="info" @click="onClickStart"> Start </v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClickStart() {
+      this.$router.push({ name: 'index' })
+    }
+  }
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center">
     <v-col>
       <v-card width="400px" class="mx-auto mt-5">
-        <v-card-title>
+        <!-- <v-card-title>
           <h1 class="display-1">Login</h1>
         </v-card-title>
         <v-card-text>
@@ -19,6 +19,14 @@
               <v-btn class="info"> Login </v-btn>
             </v-card-actions>
           </v-form>
+        </v-card-text> -->
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-card-actions>
+            <v-btn class="info" @click="onClickLogin"> Login </v-btn>
+          </v-card-actions>
         </v-card-text>
       </v-card>
     </v-col>
@@ -26,9 +34,24 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  data: () => ({
-    showPassword: false
-  })
+  layout: 'noNavigation',
+  computed: {
+    ...mapGetters(['clientId'])
+  },
+  methods: {
+    onClickLogin() {
+      // あとでstateを動的に
+      // Qiita認証
+      const scope = 'read_qiita'
+      const state = 'FEDCBA9876543210' // FIXME:毎回生成するように修正
+      const authUrl = `https://qiita.com/api/v2/oauth/authorize?client_id=${this.clientId}&scope=${scope}&state=${state}`
+      window.open(authUrl, '_blank', 'noopener noreferrer')
+    }
+  }
+  // data: () => ({
+  //   showPassword: false
+  // })
 }
 </script>
