16720ac60adef1cc4b9cedd9ac2bf3828245eddb
kiina2020 <akinasg@gmail.com>
2020-10-22 01:00:39 +0900

    delete oauth.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,6 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '~/plugins/axios.js',
-    '~/plugins/oauth.js',
     { src: '~plugins/persistedstate.js', ssr: false },
     { src: '~/plugins/cookie-storage.js' }
   ],
